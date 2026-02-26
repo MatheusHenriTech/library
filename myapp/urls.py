ef63570c116f77047e90f0745a9875f883994a43
Matheus Henrique da Silva <5331588@alunouninter.com>
2026-02-25 21:37:44 -0300

    Criei algumas views e fomulário

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import edit_book, home, create_book
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    
+    path('', home, name='home'),
+    path('book/edit/<int:pk>/', edit_book.as_view(), name="edit_book"),
+    path('book/create/', create_book.as_view(), name="create_book"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from .forms import BookForm  
 from .models import Book
+from django.views.generic.edit import UpdateView, CreateView
+from django.urls import reverse_lazy
+
 
 def home(request):
     books = Book.objects.all()
     return render(request, 'myapp/home.html', {'books': books})
 
-def register(request):
-    form = BookForm() 
-    return render(request, 'myapp/register.html', {'form': form})
 
+class edit_book(UpdateView):
+    model = Book
+    fields = ["title", "description", "author"]
+    template_name = "myapp/edit_book.html"
+    success_url = reverse_lazy('home')
+
+
+class create_book(CreateView):
+    model = Book
+    fields = ["title", "author", "description"]
+    template_name = "myapp/create_book.html"
+    success_url = reverse_lazy('home')
