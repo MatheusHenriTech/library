310101e26988dcfba302dd589ca292452f0e2cf3
Matheus Henrique da Silva <5331588@alunouninter.com>
2026-03-02 21:09:22 -0300

    Aprendendo sobre Login e Autenticação no Django

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import edit_book, home, create_book, delete_book
+from .views import edit_book, home, create_book, delete_book, register_user, login_user
 
 urlpatterns = [
     path('', home, name='home'),
     path('book/edit/<int:pk>/', edit_book.as_view(), name="edit_book"),
     path('book/create/', create_book.as_view(), name="create_book"),
     path('book/delete/<int:pk>/', delete_book.as_view(), name="delete_book"),
+    path('book/register/', register_user, name='register'),
+    path('book/login/', login_user, name='login'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,8 +4,15 @@ from .models import Book
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.urls import reverse_lazy
 from django.core.paginator import Paginator
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
+@login_required(login_url="/book/login/")
 def home(request):
     books = Book.objects.all()
 
@@ -15,21 +22,60 @@ def home(request):
     return render(request, 'myapp/home.html', {'page_obj': page_obj})
 
 
-class edit_book(UpdateView):
+class edit_book(LoginRequiredMixin, UpdateView):
+    login_url = '/book/login/'
     model = Book
     fields = ["title", "author", "description"]
     template_name = "myapp/edit_book.html"
     success_url = reverse_lazy('home')
 
 
-class create_book(CreateView):
+class create_book(LoginRequiredMixin, CreateView):
+    login_url = '/book/login/'
     model = Book
     fields = ["title", "author", "description"]
     template_name = "myapp/create_book.html"
     success_url = reverse_lazy('home')
 
 
-class delete_book(DeleteView):
+class delete_book(LoginRequiredMixin, DeleteView):
+    login_url = '/book/login/'
     model = Book
     template_name = "myapp/delete_book.html"
     success_url = reverse_lazy('home')
+
+
+def register_user(request):
+    if request.method == "GET":
+        return render(request, 'myapp/register.html')
+
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        user = User.objects.filter(username=username).first()
+    
+        if user:
+            return HttpResponse('This user already exists')
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        
+        return HttpResponse('usuário cadastrado')
+    
+
+def login_user(request):
+    if request.method == "GET":
+        return render(request, 'myapp/login.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return HttpResponse('The email or password are invalid')
